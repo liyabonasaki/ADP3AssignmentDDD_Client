1b6a3b82a0b390b277a0b9e68de0ea8769f7d4c8
Liyabona Saki <liyabonasaki@gmail.com>
2021-06-08 09:19:45 +0200

    [Complete] entity milestone

---
+++ src/test/java/za/ac/cput/Factor/DoctorTest.java
@@ -0,0 +1,39 @@
+package za.ac.cput.Factor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+class DoctorTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+    @Test
+    void testEquality(){
+
+    }
+
+    @Test
+    void testIdentity(){
+
+    }
+
+    @Test
+    void fail(){
+
+    }
+
+    @Test
+    @Timeout(5)
+    void TimeoutTest() throws  InterruptedException{
+        TimeUnit.SECONDS.sleep(4);
+        /*
+        TODO
+         */
+    }
+
+}
