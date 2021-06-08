9d50c393546204b5fe42d3c5bfbb6a1e5c76b9a9
Liyabona Saki <liyabonasaki@gmail.com>
2021-06-08 23:31:08 +0200

    [implementing ] junit test

--- src/test/java/za/ac/cput/Factor/DoctorTest.java
+++ src/test/java/za/ac/cput/Factor/DoctorTest.java
@@ -3,14 +3,22 @@ package za.ac.cput.Factor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import static org.junit.jupiter.api.Assertions.*;
+
+import za.ac.cput.Entity.Doctor;
 
 import java.util.concurrent.TimeUnit;
 
 class DoctorTest {
 
+    private Doctor.Builder build1;
+    private Doctor.Builder build2;
+
     @BeforeEach
     void setUp() {
-
+        build1 = new Doctor.Builder();
+        build2 = new Doctor.Builder();
+        build1 = build2;
     }
     @Test
     void testEquality(){
@@ -19,6 +27,7 @@ class DoctorTest {
 
     @Test
     void testIdentity(){
+        assertSame(build1 , build2);
 
     }
 
