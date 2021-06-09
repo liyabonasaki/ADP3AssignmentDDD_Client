87058108dc24c49b6283d3fa1d9754d126be9014
Joshua Luke <214234169@mycput.ac.za>
2021-06-10 01:11:07 +0200

    [Created Entity class 'Nurse']

---
+++ src/main/java/za/ac/cput/Entity/Nurse.java
@@ -0,0 +1,11 @@
+package za.ac.cput.Entity;
+
+public class Nurse {
+    private String nurseID, nurseUsername, nursePassword, nurseName, nurseGender, nurseAddress;
+    private int nurseAge, nurseContactNumber;
+
+    public static class Builder
+    {
+
+    }
+}
--- src/main/java/za/ac/cput/Entity/main.java
+++
@@ -1,8 +0,0 @@
-package za.ac.cput.Entity;
-
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("ADP3 DDD: Implementing Hospital Management System UML class ");
-    }
-}
