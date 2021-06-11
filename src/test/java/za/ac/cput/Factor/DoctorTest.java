0b82fe7eeca08f53e618b5fed06a68b546a45373
Liyabona Saki <liyabonasaki@gmail.com>
2021-06-11 09:12:23 +0200

    [Factory package] implementing TDD for the doctor class

--- src/test/java/za/ac/cput/Factor/DoctorTest.java
+++ src/test/java/za/ac/cput/Factor/DoctorTest.java
@@ -1,5 +1,13 @@
+/*
+DoctorTest.java
+Factory for doctor
+Author : Liyabona Saki (217120830)
+Date : 09 June 2021
+ */
+
 package za.ac.cput.Factor;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -19,16 +27,22 @@ class DoctorTest {
         build1 = new Doctor.Builder();
         build2 = new Doctor.Builder();
         build1 = build2;
+
     }
     @Test
     void testEquality(){
+        Assertions.assertEquals(build1, build2);
 
     }
 
     @Test
     void testIdentity(){
+        //Early exits
+        if(build1!= build2){
+            assertNotSame(build1,build2);
+            return;
+        }
         assertSame(build1 , build2);
-
     }
 
     @Test
@@ -38,11 +52,29 @@ class DoctorTest {
 
     @Test
     @Timeout(5)
-    void TimeoutTest() throws  InterruptedException{
-        TimeUnit.SECONDS.sleep(4);
-        /*
-        TODO
-         */
+    void PopulateFields() throws  InterruptedException{
+        try{
+            TimeUnit.SECONDS.sleep(4);
+            if (build1 != null){
+                build1.setid(1);
+                build1.setname("Liyabona");
+                build1.setlastname("Saki");
+                build1.setage(23);
+                build1.setaddres("Cape town");
+                build1.setgender("male");
+                build1.setphone_number("025566589");
+                build1.setusername("liyabona");
+                build1.setpassword("liyabona@98");
+
+                assertNotNull(build1.build());
+                System.out.println(build1.toString());
+            }else {
+                assertNull(build1.build());
+            }
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+
     }
 
 }
