f6a154b16ba464003f5719c1e0998ac7102d29cc
Joshua Retief <Joshua>
2021-09-27 23:25:38 +0200

    [ADDED NurseControllerTest] & [MODIFIED NurseControllerTest & NurseServiceTest]

--- src/test/java/za/ac/cput/Services/NurseServiceTest.java
+++ src/test/java/za/ac/cput/Services/NurseServiceTest.java
@@ -24,30 +24,54 @@ class NurseServiceTest {
                     26
             );
 
+    private static Nurse nurse2 = NurseFactory.createNurse
+            (
+                    "950812",
+                    "wesmichael95",
+                    "wm95_1536",
+                    "Wesley Retief",
+                    "Male",
+                    "212 WJT 8001",
+                    "0840873716",
+                    26
+            );
+
     @Test
-    void create() {
-        Nurse created = service.create(nurse);
-        assertNotNull(created);
+    void a_create()
+    {
+        Nurse created2 = service.create(nurse2);
+        assertNotNull(created2);
         //assertEquals(created.getClass(), nurse.nurseID);
-        System.out.println("Create: " + created);
+        System.out.println("Create: " + created2);
     }
 
     @Test
-    void read() {
+    void b_read() {
         Nurse read = service.read(nurse.nurseID);
         assertNotNull(read);
         System.out.println("Read: " + read);
     }
 
     @Test
-    void update() {
+    void c_update()
+    {
+        Nurse updated = new Nurse.BuilderNurse().copy(nurse).setNurseName("Melanie").build();
+        assertNotNull(service.update(updated));
+        System.out.println("Update: " + updated);
     }
 
     @Test
-    void delete() {
+    void d_delete()
+    {
+        boolean success = service.delete(nurse.nurseID);
+        assertTrue(success);
+        System.out.println("Delete: " + success);
     }
 
     @Test
-    void getAllNurse() {
+    void e_getAllNurse()
+    {
+        System.out.println("Show All: ");
+        System.out.println(service.getAllNurse());
     }
 }
