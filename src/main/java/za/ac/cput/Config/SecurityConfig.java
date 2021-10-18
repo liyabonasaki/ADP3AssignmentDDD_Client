fed0099225b4b0f5d3b3709341b9c582c858ca3c
Liyabona Saki <liyabonasaki@gmail.com>
2021-10-18 10:42:54 +0200

    Update : security conf

--- src/main/java/za/ac/cput/Config/SecurityConfig.java
+++ src/main/java/za/ac/cput/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package za.ac.cput.Config;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
